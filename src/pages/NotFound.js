01696f96ab0b9913f5768bcab0f78d6512da91dd
Anthony Lebro <antho.devjam@gmail.com>
2021-08-12 20:14:37 +0200

    Version 1.4

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {useContext} from 'react';
+
+
 
 const NotFound = () => {
   return (
     <main className='not-found'>
       <div className='not-found-content'>
         <h1>404 Not Found</h1>
-        <h1>rien a voir ici...</h1>
-        <NavLink exact to='/' className='nav-link' activeClassName='nav-link-active'>Home</NavLink>
+        <h1>La page que vous cherchez n'existe pas...</h1>
+        <div className="img">
+        </div>
       </div>
     </main>
   );
